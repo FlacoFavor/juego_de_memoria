b84358af90b2a9c58d03a3698490f8d8c7e27076
FlacoFavor <julianbarrera993@gmail.com>
2026-03-05 21:54:40 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,30 @@
-const CACHE_NAME="memory-game-cache-v1"
+const CACHE_NAME = "memory-emoji-cache-v1";
 
-const urlsToCache=[
-"/",
-"/index.html",
-"/style.css",
-"/script.js",
-"/manifest.json"
-]
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json"
+];
 
-self.addEventListener("install",event=>{
-event.waitUntil(
-caches.open(CACHE_NAME)
-.then(cache=>cache.addAll(urlsToCache))
-)
-})
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+      .catch(err => {
+        console.log("Error al cachear:", err);
+      })
+  );
+});
 
-self.addEventListener("fetch",event=>{
-event.respondWith(
-caches.match(event.request)
-.then(response=>{
-return response || fetch(event.request)
-})
-)
-})
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
